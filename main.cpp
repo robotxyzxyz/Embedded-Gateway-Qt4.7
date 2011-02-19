2f8e4679c347ce5111d34fea66dfdd60decf2a70
uranusjr <r98631012@ntu.edu.tw>
2011-02-19 16:56:31 +0800

    Use hard-coded 12-pt instead of platform-dependent ifdef and 3-timed font size (NOT TESTED)

--- main.cpp
+++ main.cpp
@@ -6,11 +6,16 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-#ifdef ARM		// Font modification hack...Pass DEFINES+=ARM to qmake to use
+/*#ifdef ARM		// Font modification hack...Pass DEFINES+=ARM to qmake to use
 	QFont f = QApplication::font();
 	f.setPointSize(f.pointSize() * 3);
 	QApplication::setFont(f);
-#endif
+#endif*/
+
+	QFont f = QApplication::font();
+	f.setPointSize(12);
+	QApplication::setFont(f);
+
     MainController controller;
 
     return a.exec();

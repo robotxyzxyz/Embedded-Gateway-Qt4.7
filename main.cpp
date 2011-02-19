3db9743fe156038c94ff0bca1630d64bd3a81af0
uranusjr <r98631012@ntu.edu.tw>
2011-02-20 00:29:30 +0800

    Hard-coded font size didn't work, switched back to platform dependent defines

--- main.cpp
+++ main.cpp
@@ -6,15 +6,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-/*#ifdef ARM		// Font modification hack...Pass DEFINES+=ARM to qmake to use
+#ifdef ARM		// Font modification hack...Pass DEFINES+=ARM to qmake to use
 	QFont f = QApplication::font();
 	f.setPointSize(f.pointSize() * 3);
 	QApplication::setFont(f);
-#endif*/
-
-	QFont f = QApplication::font();
-	f.setPointSize(12);
-	QApplication::setFont(f);
+#endif
 
     MainController controller;
 
